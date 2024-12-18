f066feab659d8053172f8a23aa24a008fa4ddfdc
Atharva-1212 <101569721+Atharva-1212@users.noreply.github.com>
2024-12-18 12:33:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin ["https://Mern1-1whq.vercel.app"]
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
 // Test Route
